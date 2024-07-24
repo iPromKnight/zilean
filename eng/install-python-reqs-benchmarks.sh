6ec8984b23fef7331f42ce30ce5c6569944c9a47
iPromKnight <promknight@proton.me>
2024-07-24 08:53:23 +0100

    fix(eng): Updated Python version for pip install in benchmark setup script from 3 to 3.11

--- eng/install-python-reqs-benchmarks.sh
+++ eng/install-python-reqs-benchmarks.sh
@@ -2,4 +2,4 @@
 
 rm -rf ../src/Zilean.Benchmarks/python
 mkdir -p ../src/Zilean.Benchmarks/python
-python3 -m pip install -r ../requirements.txt -t ../src/Zilean.Benchmarks/python/
+python3.11 -m pip install -r ../requirements.txt -t ../src/Zilean.Benchmarks/python/
