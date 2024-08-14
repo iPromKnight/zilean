7cfb090c4548a96d2436f6e111705f4d2ec1004e
iPromKnight <promknight@proton.me>
2024-08-14 23:38:30 +0100

    fix: Correct order of SQL commands in migration script

--- src/Zilean.Database/Migrations/2_ReleaseFunctionsAndIndexes.cs
+++ src/Zilean.Database/Migrations/2_ReleaseFunctionsAndIndexes.cs
@@ -16,11 +16,11 @@ public partial class ReleaseFunctionsAndIndexes : Migration
         migrationBuilder.Sql("CREATE EXTENSION IF NOT EXISTS pg_trgm;");
         migrationBuilder.Sql("SET pg_trgm.similarity_threshold = 0.85;");
 
-        migrationBuilder.Sql(SearchImdbProcedure.RemoveImdbProcedure);
-        migrationBuilder.Sql(SearchImdbProcedure.RemoveTorrentProcedure);
-        migrationBuilder.Sql(ImdbFilesIndexes.RemoveIndexes);
         migrationBuilder.Sql(OnTorrentInsertFileImdbId.RemoveTrigger);
         migrationBuilder.Sql(TorrentsFetchImdbId.RemoveProcedure);
+        migrationBuilder.Sql(SearchImdbProcedure.RemoveTorrentProcedure);
+        migrationBuilder.Sql(SearchImdbProcedure.RemoveImdbProcedure);
+        migrationBuilder.Sql(ImdbFilesIndexes.RemoveIndexes);
 
         migrationBuilder.Sql(SearchImdbProcedure.CreateImdbProcedure);
         migrationBuilder.Sql(SearchImdbProcedure.CreateTorrentProcedure);
@@ -32,11 +32,11 @@ public partial class ReleaseFunctionsAndIndexes : Migration
     /// <inheritdoc />
     protected override void Down(MigrationBuilder migrationBuilder)
     {
-        migrationBuilder.Sql(ImdbFilesIndexes.RemoveIndexes);
         migrationBuilder.Sql(OnTorrentInsertFileImdbId.RemoveTrigger);
         migrationBuilder.Sql(TorrentsFetchImdbId.RemoveProcedure);
         migrationBuilder.Sql(SearchImdbProcedure.RemoveTorrentProcedure);
         migrationBuilder.Sql(SearchImdbProcedure.RemoveImdbProcedure);
+        migrationBuilder.Sql(ImdbFilesIndexes.RemoveIndexes);
         migrationBuilder.Sql("DROP EXTENSION IF EXISTS pg_trgm;");
     }
 }
