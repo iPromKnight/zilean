a31d543eecebb96cc12ea527e5ab3c89e85f7b1b
iPromKnight <promknight@proton.me>
2025-01-18 21:25:58 +0000

    fix: Fix imdbSearch matching - thanks MatYRiCs

---
+++ src/Zilean.Database/Migrations/20250118212357_SearchImdbV3.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Zilean.Database.Functions;
+
+#nullable disable
+
+namespace Zilean.Database.Migrations;
+
+/// <inheritdoc />
+public partial class SearchImdbV3 : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql(SearchImdbProcedureV2.RemoveImdbProcedure);
+        migrationBuilder.Sql(SearchImdbProcedureV3.CreateImdbProcedure);
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql(SearchImdbProcedureV3.RemoveImdbProcedure);
+        migrationBuilder.Sql(SearchImdbProcedureV2.CreateImdbProcedure);
+    }
+}
