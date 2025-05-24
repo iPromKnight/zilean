9abc8738498fe4790422227305c19658cd57a841
Owen000 <67721175+Owen000@users.noreply.github.com>
2025-05-24 13:33:01 -0500

    Make postgres password dynamic

--- src/Zilean.Shared/Features/Configuration/DatabaseConfiguration.cs
+++ src/Zilean.Shared/Features/Configuration/DatabaseConfiguration.cs
@@ -2,5 +2,16 @@ namespace Zilean.Shared.Features.Configuration;
 
 public class DatabaseConfiguration
 {
-    public string ConnectionString { get; set; } = "Host=postgres;Database=zilean;Username=postgres;Password=postgres;Include Error Detail=true;Timeout=30;CommandTimeout=3600;";
+  public string ConnectionString { get; set; }
+
+  public DatabaseConfiguration()
+  {
+    var password = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
+    if (string.IsNullOrWhiteSpace(password))
+    {
+      throw new InvalidOperationException("Environment variable POSTGRES_PASSWORD is not set.");
+    }
+
+    ConnectionString = $"Host=postgres;Database=zilean;Username=postgres;Password={password};Include Error Detail=true;Timeout=30;CommandTimeout=3600;";
+  }
 }
